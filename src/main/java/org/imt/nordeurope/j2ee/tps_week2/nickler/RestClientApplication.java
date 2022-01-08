bf6dbfe44cc067b4ece479513e6368a2475e6c9b
GCeuninck <guillaume.ceuninck@etu.imt-lille-douai.fr>
2022-01-08 23:04:57 +0100

    Add OpenAPi Documentation

--- src/main/java/org/imt/nordeurope/j2ee/tps_week2/nickler/RestClientApplication.java
+++ src/main/java/org/imt/nordeurope/j2ee/tps_week2/nickler/RestClientApplication.java
@@ -1,5 +1,6 @@
 package org.imt.nordeurope.j2ee.tps_week2.nickler;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.imt.nordeurope.j2ee.tps_week2.nickler.Model.Countries;
 import org.imt.nordeurope.j2ee.tps_week2.nickler.Model.Temperature;
 import org.imt.nordeurope.j2ee.tps_week2.nickler.Model.TemperatureByCountry;
@@ -10,6 +11,7 @@ import org.springframework.web.client.RestTemplate;
 import java.util.List;
 
 @SpringBootApplication
+@OpenAPIDefinition()
 public class RestClientApplication {
 
 	public static void main(String[] args) {
