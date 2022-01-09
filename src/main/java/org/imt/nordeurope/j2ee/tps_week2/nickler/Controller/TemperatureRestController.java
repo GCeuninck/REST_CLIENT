b3b19598699b2a2fd649147ebe18ddb4ae8bfad9
GCeuninck <guillaume.ceuninck@etu.imt-lille-douai.fr>
2022-01-09 13:08:14 +0100

    Add index

--- src/main/java/org/imt/nordeurope/j2ee/tps_week2/nickler/Controller/TemperatureRestController.java
+++ src/main/java/org/imt/nordeurope/j2ee/tps_week2/nickler/Controller/TemperatureRestController.java
@@ -1,6 +1,7 @@
 package org.imt.nordeurope.j2ee.tps_week2.nickler.Controller;
 
 import org.imt.nordeurope.j2ee.tps_week2.nickler.Model.TemperatureByCountry;
+import org.imt.nordeurope.j2ee.tps_week2.nickler.Model.TemperatureForm;
 import org.imt.nordeurope.j2ee.tps_week2.nickler.Service.ITemperatureRestService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,10 +16,21 @@ public class TemperatureRestController {
     @Inject
     ITemperatureRestService service;
 
-    @GetMapping(value = {"/", "/temperature"})
+    @GetMapping(value = {"/"})
+    public String index(Model model) {
+        model.addAttribute("temperatureForm", new TemperatureForm());
+        return "index";
+    }
+
+    @GetMapping(value = {"/temperature"})
     public String getAll(Model model) {
         model.addAttribute("countries", service.getAll());
         return "temperature";
+    }
+
+    @PostMapping(value = {"/temperature/search"})
+    public String getCountryForm(Model model, @ModelAttribute("temperatureForm") TemperatureForm temperatureForm) {
+        return "redirect:/temperature/" + temperatureForm.getCountry();
     }
 
     @GetMapping(value = "/temperature/{country}")
---
+++ src/main/java/org/imt/nordeurope/j2ee/tps_week2/nickler/Model/TemperatureForm.java
@@ -0,0 +1,13 @@
+package org.imt.nordeurope.j2ee.tps_week2.nickler.Model;
+
+public class TemperatureForm {
+    private String country;
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+}
